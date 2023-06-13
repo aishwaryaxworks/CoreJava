512bba69e185d8e4524761ad78225486a2cffb77
aishwaryaxworks <aishwaryanaik.xworks@gmail.com>
2023-06-13 21:18:09 +0530

    Constructor Example

---
+++ constructor/runner/PresentationRunner.java
@@ -0,0 +1,150 @@
+package com.xworkz.constructor.runner;
+
+import com.xworkz.constructor.things.Presentation;
+
+public class PresentationRunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Presentation present = new Presentation();
+		
+		System.out.println("Instance 1 -----------");
+		
+		present.topic="The Impact of Artificial Intelligence on Modern Society";
+		present.presentor="Amelia Thompson";
+		present.numOfSlides=20;
+		present.audience=35;
+		present.place="Chandrapur";
+		present.breaksGiven=false;
+		present.foodProvision=true;
+		present.organiser="Ethan Mitchell";
+		
+		System.out.println("Presentation Topic:"+present.topic+" & Presentor:"+present.presentor+". "
+				+ "Organiser is:"+present.organiser);
+		System.out.println("No. of Slides:"+present.numOfSlides);
+		System.out.println("Audience Size:"+present.audience+" are in Place:"+present.place);
+		System.out.println("Are breaks given:"+present.breaksGiven+" & is Food previded:"+present.foodProvision);
+		
+		System.out.println("------------------------------");
+		
+		System.out.println("Instance 2 -----------");
+		
+		present.topic="Sustainable Practices for a Greener Future";
+		present.presentor="Benjamin Rodriguez";
+		present.numOfSlides=30;
+		present.audience=60;
+		present.place="Eldridge Bay";
+		present.breaksGiven=true;
+		present.foodProvision=false;
+		present.organiser="Harper Davis";
+		
+		System.out.println("Presentation Topic:"+present.topic+" & Presentor:"+present.presentor+". "
+				+ "Organiser is:"+present.organiser);
+		System.out.println("No. of Slides:"+present.numOfSlides);
+		System.out.println("Audience Size:"+present.audience+" are in Place:"+present.place);
+		System.out.println("Are breaks given:"+present.breaksGiven+" & is Food previded:"+present.foodProvision);
+		
+		System.out.println("------------------------------");
+		
+		System.out.println("Instance 3 -----------");
+		
+		present.topic="Exploring the Potential of Virtual Reality in Education";
+		present.presentor="Emily Patel";
+		present.numOfSlides=40;
+		present.audience=50;
+		present.place="Belagavi";
+		present.breaksGiven=true;
+		present.foodProvision=true;
+		present.organiser="Stella Morgan";
+		
+		System.out.println("Presentation Topic:"+present.topic+" & Presentor:"+present.presentor+". "
+				+ "Organiser is:"+present.organiser);
+		System.out.println("No. of Slides:"+present.numOfSlides);
+		System.out.println("Audience Size:"+present.audience+" are in Place:"+present.place);
+		System.out.println("Are breaks given:"+present.breaksGiven+" & is Food previded:"+present.foodProvision);
+		
+		System.out.println("------------------------------");
+		
+		System.out.println("Instance 4 -----------");
+		
+		present.topic="Cybersecurity: Protecting Your Digital Identity";
+		present.presentor="Daniel Carter";
+		present.numOfSlides=10;
+		present.audience=20;
+		present.place="Havenbrook";
+		present.breaksGiven=false;
+		present.foodProvision=false;
+		present.organiser="Victoria Green";
+		
+		System.out.println("Presentation Topic:"+present.topic+" & Presentor:"+present.presentor+". "
+				+ "Organiser is:"+present.organiser);
+		System.out.println("No. of Slides:"+present.numOfSlides);
+		System.out.println("Audience Size:"+present.audience+" are in Place:"+present.place);
+		System.out.println("Are breaks given:"+present.breaksGiven+" & is Food previded:"+present.foodProvision);
+		
+		System.out.println("------------------------------");
+		
+		System.out.println("Instance 5 -----------");
+		
+		present.topic="The Power of Emotional Intelligence in Leadership";
+		present.presentor="Nisha Desai";
+		present.numOfSlides=54;
+		present.audience=70;
+		present.place="Gorakhpur";
+		present.breaksGiven=true;
+		present.foodProvision=false;
+		present.organiser="Olivia Johnson";
+		
+		System.out.println("Presentation Topic:"+present.topic+" & Presentor:"+present.presentor+". "
+				+ "Organiser is:"+present.organiser);
+		System.out.println("No. of Slides:"+present.numOfSlides);
+		System.out.println("Audience Size:"+present.audience+" are in Place:"+present.place);
+		System.out.println("Are breaks given:"+present.breaksGiven+" & is Food previded:"+present.foodProvision);
+		
+		System.out.println("------------------------------");
+		
+		System.out.println("Instance 6 -----------");
+		
+		present.topic="Unveiling the Mysteries of Dark Matter and Dark Energy";
+		present.presentor="Aditi Sharma";
+		present.numOfSlides=50;
+		present.audience=80;
+		present.place="Dhanbad";
+		present.breaksGiven=true;
+		present.foodProvision=true;
+		present.organiser="Caleb Ramirez";
+		
+		System.out.println("Presentation Topic:"+present.topic+" & Presentor:"+present.presentor+". "
+				+ "Organiser is:"+present.organiser);
+		System.out.println("No. of Slides:"+present.numOfSlides);
+		System.out.println("Audience Size:"+present.audience+" are in Place:"+present.place);
+		System.out.println("Are breaks given:"+present.breaksGiven+" & is Food previded:"+present.foodProvision);
+		
+		System.out.println("------------------------------");
+		
+		System.out.println("Instance 7 -----------");
+		
+		present.topic="The Evolution of Social Media and Its Influence on Communication";
+		present.presentor="Meera Singh";
+		present.numOfSlides=10;
+		present.audience=35;
+		present.place="Udupi";
+		present.breaksGiven=false;
+		present.foodProvision=false;
+		present.organiser="Leo Cooper";
+		
+		System.out.println("Presentation Topic:"+present.topic+" & Presentor:"+present.presentor+". "
+				+ "Organiser is:"+present.organiser);
+		System.out.println("No. of Slides:"+present.numOfSlides);
+		System.out.println("Audience Size:"+present.audience+" are in Place:"+present.place);
+		System.out.println("Are breaks given:"+present.breaksGiven+" & is Food previded:"+present.foodProvision);
+		
+		System.out.println("------------------------------");
+		
+		
+		
+
+
+	}
+
+}
---
+++ constructor/things/Presentation.java
@@ -0,0 +1,17 @@
+package com.xworkz.constructor.things;
+
+public class Presentation {
+	
+// default constructor 
+	
+	//instance variables
+	public String topic;
+	public String presentor;
+	public int numOfSlides;
+	public int audience;
+	public String place;
+	public boolean breaksGiven;
+	public boolean foodProvision;
+	public String organiser;
+
+}
